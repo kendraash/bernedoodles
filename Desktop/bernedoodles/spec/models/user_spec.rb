b00d2ae676109eb146a7690d059cc981fce30757
Kendra Ash <kendra.ash32@gmail.com>
2015-10-23 10:09:13 -0700

    add unit testing for User class, validates attributes and relationships

--- Desktop/bernedoodles/spec/models/user_spec.rb
+++ Desktop/bernedoodles/spec/models/user_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 describe User do
+  it { should validate_presence_of :email}
+  it { should validate_presence_of :password}
+  it { should have_many :dogs}
+  it { should have_many :litters}
+  it { should have_many :puppies}
 end
