6fb83a32747821a47808c35a08d9089ad6aeb0b9
Kendra Ash <kendra.ash32@gmail.com>
2015-10-23 10:06:35 -0700

    add unit testing for puppies that validates attributes and relationships

--- Desktop/bernedoodles/app/models/litter.rb
+++ Desktop/bernedoodles/app/models/litter.rb
@@ -2,4 +2,5 @@ class Litter<ActiveRecord::Base
   validates_presence_of :due_date
   belongs_to :user
   belongs_to :dog
+  has_many :puppies
 end
--- Desktop/bernedoodles/spec/models/litter_spec.rb
+++ Desktop/bernedoodles/spec/models/litter_spec.rb
@@ -4,5 +4,6 @@ describe Litter do
   it { should validate_presence_of :due_date}
   it { should belong_to :user}
   it { should belong_to :dog}
+  it { should have_many :puppies }
 
 end
