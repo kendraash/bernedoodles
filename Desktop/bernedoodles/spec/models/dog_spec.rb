4053040902ab1e642737dc8ced2975472c520d21
Kendra Ash <kendra.ash32@gmail.com>
2015-10-23 09:58:24 -0700

    passing specs for Dog class

---
+++ Desktop/bernedoodles/spec/models/dog_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Dog do
+  it { should validate_presence_of :name}
+  it { should validate_presence_of :date_of_birth}
+  it { should validate_presence_of :sex}
+  it { should have_many :litters}
+
+end
