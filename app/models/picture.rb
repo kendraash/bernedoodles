c9b998f9f924ae66c89a9a03a10304ccd71c0574
Kendra Ash <student@epicodus.com>
2015-11-02 15:05:18 -0800

    add styling to picture size

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
-  has_attached_file :name, :styles => { :medium => "300x300>" }
+  has_attached_file :name, :styles => { :medium => "300x300#" }
   validates_attachment_content_type :name, content_type: /\Aimage\/.*\Z/
 end
