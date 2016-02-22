358eacd5f0d45e433501e985375b99e2d9d3d715
Kendra Ash <kendra.ash32@gmail.com>
2016-02-21 18:41:54 -0800

    update configurations in production for paperclip
    # Conflicts:
    #	Gemfile
    #	Gemfile.lock
    #	config/environments/production.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
-  has_attached_file :name, :styles => { :medium => "300x300#" },
-        :default_url => "****",
-        :storage => :s3,
-        :bucket => '****',
-        :s3_credentials => "../../config/secrets.yml",
-            :url => :s3_path_url,
-            :path => ":image/:id/:style/:basename.:extension"
+  has_attached_file :name, :styles => { :medium => "300x300#" }
   validates_attachment_content_type :name, content_type: /\Aimage\/.*\Z/
 end
--- config/initializers/s3.rb
+++
@@ -1,5 +0,0 @@
-#config/initializers/s3.rb
- if Rails.env == "production"
-     # get credentials from YML file
-     S3_CREDENTIALS = Rails.root.join("config/secrets.yml")
- end
