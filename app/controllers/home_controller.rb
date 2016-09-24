aee0054eb5a98f23a57de6ab8cce09b547ba78d5
Kendra Ash <kendra.ash32@gmail.com>
2016-09-23 20:21:06 -0700

    change the limit of pictures in the carousel, and shuffle pictures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-     @pictures = Picture.all
-     @Lists = List.all 
+     @pictures = Picture.all.shuffle
+     temp = @pictures.length
+     array_size = temp -  20
+     @pictures = @pictures.drop(array_size)
+     @Lists = List.all
    end
 end
